ed9717e6d0c1dcfb592e8f2ed8ee7e77d2c2a19d
Mathew Hall <m@mhall.xyz>
2018-06-14 21:08:59 +0100

    Main page added

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 app.on('ready', () => {
     mainWindow = new BrowserWindow;
-    mainWindow.loadURL('https://mhall.xyz');
+    mainWindow.loadURL(`file:${__dirname}/main.html`);
 })
